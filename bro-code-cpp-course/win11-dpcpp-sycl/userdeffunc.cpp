55c0298d2bd31a0b015a8e086e1ce7ab033e80c9
badvibecoder <248558263+badvibecoder@users.noreply.github.com>
2026-06-23 23:21:28 -0500

    practice

--- bro-code-cpp-course/win11-dpcpp-sycl/userdeffunc.cpp
+++ bro-code-cpp-course/win11-dpcpp-sycl/userdeffunc.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 
+// put user made funcs before main()
+
+void myFunc(){
+	std::cout << "My custom function\n";
+}
+
+// We can also declare funcs here, but write them later in the prog
+
+void myOtherFunc();
+
+// Pass the arg
+
+void myPassArgsFunc(std::string named){
+
+	std::cout << "Hello: " << named << "\n";
+
+}
+
 int main() {
+
+	std::string name = "Chanche";
 
 	std::cout << "\n\nUser Defined Functions\n\n";
 
+	myFunc();
+
+	myOtherFunc();
+
+	myPassArgsFunc(name);
+
 	return 0;
+}
+
+void myOtherFunc(){
+	std::cout <<  "This func is at the bottom\n";
 }
